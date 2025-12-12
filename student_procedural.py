64ab6145b6d421feda8475b97acc473cbb6faff9
Ebenezer Fuachie <fuachiee717@gmail.com>
2025-12-12 21:31:56 +0000

    File: renamed student.py to student_procedural

---
+++ student_procedural.py
@@ -0,0 +1,7 @@
+"""
+This file have the solution implemented using the procedural paradigm
+"""
+
+class Student:
+    def __init__(self, name, age, program_of_study, hall_of_residence):
+        pass
